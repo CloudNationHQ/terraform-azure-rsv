acd69178db68325d7364bfcd9a8f531678336f40
dkooll <dennis.kool@cloudnation.nl>
2025-05-14 14:55:06 +0200

    feat: fixing linting issues

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "vault" {
   description = "Contains all recovery services vault configuration"
-  value = azurerm_recovery_services_vault.vault
+  value       = azurerm_recovery_services_vault.vault
 }
